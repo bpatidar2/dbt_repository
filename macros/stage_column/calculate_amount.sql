02acfa4cc67a79f38a1d83488010e90a8b52a159
Brijmohan Patidar <mnutrifood@gmail.com>
2025-11-07 12:41:02 +0000

    commit

---
+++ macros/stage_column/calculate_amount.sql
@@ -0,0 +1,22 @@
+{% macro calculate_amount(col1,col2) %}
+ {{col1}}+{{col2}}   
+{% endmacro %}
+
+
+->concat_contract_date_id.sql
+
+{% macro contract_date_id(contract_column, schedule_column, date_column, add_days) -%}
+
+
+concat( {{ contract_column }}, '-',
+
+   {%- if  schedule_column|length > 1 -%}
+      {%- if 'contract_column' != '0' -%}
+         {{ schedule_column }},'-',
+      {%- endif -%}
+   {%- endif -%}
+
+    replace(DATEADD(D,{{ add_days }},{{ date_column }}) ,'-',''))::varchar(50)
+
+
+{%- endmacro %}
